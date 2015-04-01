b00dd49c2e09d96b7a9e0a2f46ad5af7be2ea049
Sigit Nugraha <sigit.nugraha@gesis.org>
2015-04-01 20:55:42 +0200

    reformated style

--- src/main/java/de/rwth/i9/palm/controller/DatasetController.java
+++ src/main/java/de/rwth/i9/palm/controller/DatasetController.java
@@ -4,11 +4,13 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import de.rwth.i9.palm.model.Algorithm;
 import de.rwth.i9.palm.persistence.PersistenceStrategy;
 
 @Controller
@@ -20,12 +22,21 @@ public class DatasetController
 	private PersistenceStrategy persistenceStrategy;
 
 	@RequestMapping( method = RequestMethod.GET )
+	@Transactional
 	public ModelAndView landing( @RequestParam( value = "sessionid", required = false ) final String sessionId, final HttpServletResponse response ) throws InterruptedException
 	{
 		ModelAndView model = new ModelAndView( "dataset", "link", "dataset" );
 
 		if ( sessionId != null && sessionId.equals( "0" ) )
 			response.setHeader( "SESSION_INVALID", "yes" );
+
+		// Publication pub = new Publication();
+		// pub.setAbstractOriginal( "something" );
+		// persistenceStrategy.getPublicationDAO().persist( pub );
+
+		Algorithm alg = new Algorithm();
+		alg.setDescription( "something" );
+		persistenceStrategy.getAlgorithmDAO().persist( alg );
 
 		return model;
 	}
