f61165b6264e5a7433b05414ae48bb114f606771
Sigit Nugraha <sigit.nugraha@gesis.org>
2015-04-30 12:01:16 +0200

    added dirty code of itex custom pdf extraction

--- src/main/java/de/rwth/i9/palm/controller/AnalyticsController.java
+++ src/main/java/de/rwth/i9/palm/controller/AnalyticsController.java
@@ -3,11 +3,17 @@ package de.rwth.i9.palm.controller;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
+import java.util.regex.Pattern;
 
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.tika.exception.TikaException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,6 +26,9 @@ import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
 import org.xml.sax.SAXException;
 
+import de.rwth.i9.palm.analytics.api.PalmAnalytics;
+import de.rwth.i9.palm.helper.OpenNlpResultHelper;
+import de.rwth.i9.palm.helper.PdfDetail;
 import de.rwth.i9.palm.helper.PdfParser;
 import de.rwth.i9.palm.model.Publication;
 import de.rwth.i9.palm.persistence.PersistenceStrategy;
@@ -30,11 +39,14 @@ import de.rwth.i9.palm.persistence.PersistenceStrategy;
 public class AnalyticsController
 {
 
+	// logger
+	static private final Logger logger = LoggerFactory.getLogger( AnalyticsController.class );
+
 	@Autowired
 	private PersistenceStrategy persistenceStrategy;
 
-	/*@Autowired
-	private PalmAnalytics analytics;*/
+	@Autowired
+	private PalmAnalytics palmAnalytics;
 
 	@RequestMapping( value = "/dialog", method = RequestMethod.GET )
 	@Transactional
@@ -44,6 +56,39 @@ public class AnalyticsController
 
 		if ( sessionId != null && sessionId.equals( "0" ) )
 			response.setHeader( "SESSION_INVALID", "yes" );
+
+		return model;
+	}
+
+	@RequestMapping( value = "/run", method = RequestMethod.POST )
+	@Transactional
+	public ModelAndView runAnalytics( final HttpServletResponse response, @RequestParam( value = "content" ) final String stringContent )
+	{
+		ModelAndView model = new ModelAndView( "getAnalyticsResult", "link", "analytics" );
+		model.addObject( "stringContent", stringContent );
+
+		String[] sentences = palmAnalytics.getOpenNLPTool().detectSentences( stringContent );
+		List<String> listSentences = Arrays.asList( sentences );
+		List<OpenNlpResultHelper> onrhs = new ArrayList<OpenNlpResultHelper>();
+		model.addObject( "listSentences", listSentences );
+
+		for ( String sentence : sentences )
+		{
+			OpenNlpResultHelper onrh = new OpenNlpResultHelper();
+			String[] tokenizeSentence = palmAnalytics.getOpenNLPTool().tokenize( sentence );
+			String[] posTaggerSentence = palmAnalytics.getOpenNLPTool().tagPartOfSpeech( tokenizeSentence );
+			List<String> nounPhraseChunked = palmAnalytics.getOpenNLPTool().nounPhraseExtractor( tokenizeSentence, posTaggerSentence );
+
+
+			onrh.setSentence( sentence );
+			onrh.setTokenizedSentence( Arrays.asList( tokenizeSentence ) );
+			onrh.setPosTaggedSentence( Arrays.asList( posTaggerSentence ) );
+			onrh.setNounPhraseChunked( nounPhraseChunked );
+
+			onrhs.add( onrh );
+		}
+
+		model.addObject( "onrhs", onrhs );
 
 		return model;
 	}
@@ -100,8 +145,24 @@ public class AnalyticsController
 
 			File convFile = convert( mpf );
 
-			PdfParser.parsePdf( convFile );
+			PdfDetail pdfDetail = PdfParser.parsePdf( convFile );
 
+			List<String> chunkedFile = PdfParser.parsePdfPerChunks( convFile );
+
+			int i = 0;
+
+			String patternString = "[\\r\\n]+abstract";
+			Pattern pattern = Pattern.compile( patternString, Pattern.CASE_INSENSITIVE );
+
+			String[] split = pattern.split( chunkedFile.get( 0 ) );
+
+			System.out.println( "split.length = " + split.length );
+
+			for ( String element : split )
+			{
+				System.out.println( "element = " + element );
+				System.out.println( "==============================================" );
+			}
 			if ( saveToDb )
 			{
 				// persist publication here
