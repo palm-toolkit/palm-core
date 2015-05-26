dde9535e02b49ef0db6c38496edb980888feabfd
Sigit Nugraha <sigit.nugraha@gesis.org>
2015-05-26 18:10:19 +0200

    introduced templateHelper to ModelandView

--- src/main/java/de/rwth/i9/palm/controller/administration/AdministrationController.java
+++ src/main/java/de/rwth/i9/palm/controller/administration/AdministrationController.java
@@ -9,12 +9,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import de.rwth.i9.palm.helper.TemplateHelper;
 import de.rwth.i9.palm.persistence.PersistenceStrategy;
+import de.rwth.i9.palm.service.ApplicationContextService;
 
 @Controller
 @RequestMapping( value = "/admin" )
 public class AdministrationController
 {
+
+	private static final String LINK_NAME = "administration";
+
+	@Autowired
+	private ApplicationContextService appService;
 
 	@Autowired
 	private PersistenceStrategy persistenceStrategy;
@@ -22,10 +29,12 @@ public class AdministrationController
 	@RequestMapping( method = RequestMethod.GET )
 	public ModelAndView landing( @RequestParam( value = "sessionid", required = false ) final String sessionId, final HttpServletResponse response ) throws InterruptedException
 	{
-		ModelAndView model = new ModelAndView( "administration", "link", "administration" );
+		ModelAndView model = TemplateHelper.createViewWithLink( "administration", LINK_NAME );
 
 		if ( sessionId != null && sessionId.equals( "0" ) )
 			response.setHeader( "SESSION_INVALID", "yes" );
+
+		model.addObject( "activeMenu", "widget-add" );
 
 		return model;
 	}
