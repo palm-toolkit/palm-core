14ca94a35e9804d2cdf489b33d7bb2694dce7349
Sigit Nugraha <sigit.nugraha@gesis.org>
2015-06-15 00:59:15 +0200

    updated conference controller and addded conference comparator by notation

---
+++ src/main/java/de/rwth/i9/palm/helper/comparator/ConferenceByNotationComparator.java
@@ -0,0 +1,23 @@
+package de.rwth.i9.palm.helper.comparator;
+
+import java.util.Comparator;
+
+import de.rwth.i9.palm.model.Conference;
+
+/**
+ * @author matthaeus
+ * 
+ */
+public class ConferenceByNotationComparator implements Comparator<Conference>
+{
+
+	@Override
+	public int compare( final Conference conference1, final Conference conference2 )
+	{
+		String notation1 = conference1.getConferenceGroup().getNotation() + conference1.getYear();
+		String notation2 = conference2.getConferenceGroup().getNotation() + conference2.getYear();
+
+		return notation1.compareTo( notation2 );
+	}
+
+}
