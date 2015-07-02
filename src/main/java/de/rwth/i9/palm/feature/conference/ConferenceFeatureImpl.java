18a3dc03880d2b2357a85c320ef7af5b19437b39
Sigit Nugraha <sigit.nugraha@gesis.org>
2015-07-02 17:26:45 +0200

    added interface and implementation initial for researcher feature

---
+++ src/main/java/de/rwth/i9/palm/feature/conference/ConferenceFeatureImpl.java
@@ -0,0 +1,6 @@
+package de.rwth.i9.palm.feature.conference;
+
+public class ConferenceFeatureImpl implements ConferenceFeature
+{
+
+}
---
+++ src/main/java/de/rwth/i9/palm/feature/researcher/ResearcherInterestImpl.java
@@ -0,0 +1,8 @@
+package de.rwth.i9.palm.feature.researcher;
+
+import de.rwth.i9.palm.feature.AcademicFeatureImpl;
+
+public class ResearcherInterestImpl extends AcademicFeatureImpl implements ResearcherInterest
+{
+	
+}
