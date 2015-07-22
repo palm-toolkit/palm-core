731914ef9bba55551ee2f1c2da8b8a9a46405dfe
Sigit Nugraha <sigit.nugraha@gesis.org>
2015-07-22 19:47:43 +0200

    prepared other collection ans extraction services, DBLP, yahoo content analysis, mendeley

--- src/main/java/de/rwth/i9/palm/datasetcollect/service/MendeleyPublicationCollection.java
+++ src/main/java/de/rwth/i9/palm/datasetcollect/service/MendeleyPublicationCollection.java
@@ -2,5 +2,24 @@ package de.rwth.i9.palm.datasetcollect.service;
 
 public class MendeleyPublicationCollection
 {
-
+	/**
+	 * Logged in as sigit nugraha
+Display name
+sigit nugraha
+Id
+cbebc27d-600d-35db-9e57-1ecbc24229de
+Firstname
+sigit
+Lastname
+nugraha
+Email
+study.sigit@gmail.com
+Academic Status
+Student (Master)
+oAuth info
+Access token
+MSwxNDM3NTg5NzI0Mjk3LDQ0NTI3OTA2MSwxMDI4LGFsbCwsRmd3RmhaUktBUkpOM192Uk1YeGp6Zmt0S1BJ
+Refresh token
+MSw0NDUyNzkwNjEsMTAyOCxhbGwsLCwsRjVNNU1HZ0UtbTB2d01yM3NjcHNXTGl0TUVr>
+	 */
 }
