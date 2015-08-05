35d528ef13572e2318b10a67ba6ba3d7de6b0460
Sigit Nugraha <sigit.nugraha@gesis.org>
2015-08-05 11:42:51 +0200

    removed ExtractionRuntime

--- src/test/java/de/rwth/i9/palm/config/DatabaseConfigCoreTest.java
+++ src/test/java/de/rwth/i9/palm/config/DatabaseConfigCoreTest.java
@@ -24,7 +24,6 @@ import de.rwth.i9.palm.model.AuthorAlias;
 import de.rwth.i9.palm.model.Conference;
 import de.rwth.i9.palm.model.ConferenceGroup;
 import de.rwth.i9.palm.model.Dataset;
-import de.rwth.i9.palm.model.ExtractionRuntime;
 //import de.rwth.i9.palm.analytics.api.PalmAnalyticsImpl;
 import de.rwth.i9.palm.model.ExtractionService;
 import de.rwth.i9.palm.model.Function;
@@ -131,7 +130,6 @@ public class DatabaseConfigCoreTest
 						Conference.class,
 						ConferenceGroup.class,
 						Dataset.class,
-						ExtractionRuntime.class, 
 						ExtractionService.class,
 						Function.class,
 						Institution.class,
