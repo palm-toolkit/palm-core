08112fb5ea3fe8b9349ac7079e0e155b08705da5
Sigit Nugraha <sigit.nugraha@gesis.org>
2015-08-13 07:06:00 +0200

    Update: LoginController added failed Auth handling

--- src/main/java/de/rwth/i9/palm/controller/LoginController.java
+++ src/main/java/de/rwth/i9/palm/controller/LoginController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping( value = "/login" )
@@ -12,11 +13,19 @@ public class LoginController
 {
 
 	@RequestMapping( method = RequestMethod.GET )
-	public String login(
-			@RequestParam(value="form", required = false) String formMode)
+	public ModelAndView login(
+			@RequestParam(value="form", required = false) String formMode,
+			@RequestParam(value="auth", required = false) String auth)
 	{
-		if( formMode != null && formMode.equals( "true" ) )
-			return "loginForm";
-		return "login";
+		ModelAndView mav = null;
+		if ( formMode != null && formMode.equals( "true" ) )
+			mav = new ModelAndView( "loginForm" );
+		else
+			mav = new ModelAndView( "login" );
+		
+		if( auth != null && auth.equalsIgnoreCase( "fail" ))
+			mav.addObject( "auth", "fail" );
+		
+		return mav;
 	}
 }
