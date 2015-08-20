bd52758d6e5505be17b0d68152357af3ea866d40
Sigit Nugraha <sigit.nugraha@gesis.org>
2015-08-21 01:27:24 +0200

    add; Connection.Response getConnectionResponseWithJsoup

--- src/main/java/de/rwth/i9/palm/datasetcollect/service/PublicationCollectionHelper.java
+++ src/main/java/de/rwth/i9/palm/datasetcollect/service/PublicationCollectionHelper.java
@@ -28,7 +28,7 @@ public class PublicationCollectionHelper
 						.timeout( timeout );
 			
 			if( cookies != null )
-				jsoupConnection .cookies( cookies );
+				jsoupConnection.cookies( cookies );
 			
 			// get the html page
 			document = jsoupConnection.get();
@@ -41,6 +41,33 @@ public class PublicationCollectionHelper
 		}
 		
 		return document;
+	}
+	
+	public static Connection.Response getConnectionResponseWithJsoup( String url, int timeout, Map<String, String> cookies){
+		Connection.Response response = null;
+		try
+		{
+			Connection jsoupConnection = 
+					 Jsoup
+						.connect( url )
+						.userAgent( "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.130 Safari/537.36" )
+						.header( "Accept", "Accept:text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" )
+						.timeout( timeout );
+			
+			if( cookies != null )
+				jsoupConnection.cookies( cookies );
+			
+			// get the html page
+			response = jsoupConnection.execute();
+		}
+		catch ( Exception e )
+		{
+			log.debug( e.toString() );
+			log.info( e.toString() );
+			return null;
+		}
+		
+		return response;
 	}
 	
 }
