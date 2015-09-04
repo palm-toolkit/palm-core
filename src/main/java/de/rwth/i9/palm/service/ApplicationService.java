d25f0294369c6f71811715a61e7bfa578b1ab2ed
Sigit Nugraha <sigit.nugraha@gesis.org>
2015-09-04 07:37:09 +0200

    Update: Source cache from List into Map

--- src/main/java/de/rwth/i9/palm/service/ApplicationService.java
+++ src/main/java/de/rwth/i9/palm/service/ApplicationService.java
@@ -1,13 +1,17 @@
 package de.rwth.i9.palm.service;
 
+import java.util.Map;
+
 import javax.annotation.PostConstruct;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 
+import de.rwth.i9.palm.model.Source;
 import de.rwth.i9.palm.persistence.PersistenceStrategy;
 
 @Service
@@ -21,14 +25,34 @@ public class ApplicationService
 	@Autowired( required = false )
 	private FreeMarkerConfigurer freemarkerConfiguration;
 
+	// caching the sources
+	private Map<String, Source> academicNetworkSourcesCache;
+
 	@PostConstruct
 	public void init()
 	{
 		log.info( "Initializing..." );
+
+		// this.getAcademicNetworkSources();
 
 		if ( freemarkerConfiguration != null )
 		{
 			log.info( "freemarkerConfiguration" );
 		}
 	}
+
+	@Transactional
+	public Map<String, Source> getAcademicNetworkSources()
+	{
+		if ( academicNetworkSourcesCache == null || academicNetworkSourcesCache.isEmpty() )
+			updateAcademicNetworkSourcesCache();
+		return academicNetworkSourcesCache;
+	}
+
+	@Transactional
+	public void updateAcademicNetworkSourcesCache()
+	{
+		academicNetworkSourcesCache = persistenceStrategy.getSourceDAO().getSourceMap();
+	}
+
 }
