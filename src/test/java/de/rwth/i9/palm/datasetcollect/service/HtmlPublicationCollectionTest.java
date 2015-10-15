3040277ad1dacf121f117a1db77dc279df8c782d
Sigit Nugraha <sigit.nugraha@gesis.org>
2015-10-15 18:24:34 +0200

    Fix Html extraction error on some sites

--- src/test/java/de/rwth/i9/palm/datasetcollect/service/HtmlPublicationCollectionTest.java
+++ src/test/java/de/rwth/i9/palm/datasetcollect/service/HtmlPublicationCollectionTest.java
@@ -17,7 +17,6 @@ public class HtmlPublicationCollectionTest
 {
 
 	@Test
-	@Ignore
 	public void getPublicationDetailByPublicationUrlTest() throws IOException
 	{
 		String input = "http://www.scitepress.org/DigitalLibrary/Link.aspx?doi=10.5220/0004791400090020";
@@ -31,7 +30,9 @@ public class HtmlPublicationCollectionTest
 		// input = "http://link.springer.com/chapter/10.1007%2F11575801_18";
 		// input = "http://www.computer.org/csdl/proceedings/icalt/2014/4038/00/4038a044-abs.html";
 		// input = "http://dx.doi.org/10.1504/IJMLO.2010.029952";
-		input = "http://www.igi-global.com/article/toward-personal-learning-environment-framework/48222";
+		// input = "http://www.igi-global.com/article/toward-personal-learning-environment-framework/48222";
+		// input = "http://ieeexplore.ieee.org/xpl/articleDetails.jsp?arnumber=4032537";
+		input = "http://dl.acm.org/citation.cfm?id=2337376";
 
 		Map<String, String> publicationDetailMaps = HtmlPublicationCollection.getPublicationInformationFromHtmlPage( input );
 
