6f99a911bb9bb6d62f4e08e615a0ba05f9d6f094
Sigit Nugraha <sigit.nugraha@gesis.org>
2015-12-16 16:38:48 +0100

    Fix eventByYearAndVolume comparator

--- src/main/java/de/rwth/i9/palm/helper/comparator/EventByYearComparator.java
+++ src/main/java/de/rwth/i9/palm/helper/comparator/EventByYearComparator.java
@@ -10,10 +10,35 @@ public class EventByYearComparator implements Comparator<Event>
 	@Override
 	public int compare( final Event event1, final Event event2 )
 	{
-		String notation1 = event1.getYear();
-		String notation2 = event2.getYear();
+		if ( event1 == null && event2 == null )
+			return 0;
 
-		return notation1.compareTo( notation2 ) * -1;
+		if ( event1 == null )
+			return 1;
+
+		if ( event2 == null )
+			return -1;
+
+		String year1 = event1.getYear();
+		String year2 = event2.getYear();
+
+		if ( year1.equals( year2 ) )
+		{
+			if ( event1.getVolume() == null && event2.getVolume() == null )
+				return 0;
+
+			if ( event1.getVolume() == null )
+				return 1;
+
+			if ( event2.getVolume() == null )
+				return -1;
+
+			return event1.getVolume().compareTo( event2.getVolume() );
+		}
+		else
+		{
+			return year1.compareTo( year2 ) * -1;
+		}
 	}
 
 }
