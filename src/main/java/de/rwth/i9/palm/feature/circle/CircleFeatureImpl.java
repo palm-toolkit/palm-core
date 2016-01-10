1e6df86faecaf48b6edc7e77a377a563c6dfb470
Sigit Nugraha <sigit.nugraha@gesis.org>
2016-01-10 19:09:24 +0100

    Updated circle features

---
+++ src/main/java/de/rwth/i9/palm/feature/circle/CircleFeatureImpl.java
@@ -0,0 +1,60 @@
+package de.rwth.i9.palm.feature.circle;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class CircleFeatureImpl implements CircleFeature
+{
+
+	@Autowired( required = false )
+	private CircleApi circleApi;
+
+	@Autowired( required = false )
+	private CircleBasicInformation circleBasicInformation;
+
+	@Autowired( required = false )
+	private CircleDetail circleDetail;
+
+	@Autowired( required = false )
+	private CircleManage circleManage;
+
+	@Autowired( required = false )
+	private CircleSearch circleSearch;
+
+	@Override
+	public CircleApi getCircleApi()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CircleBasicInformation getCircleBasicInformation()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CircleDetail getCircleDetail()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CircleManage getCircleManage()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CircleSearch getCircleSearch()
+	{
+		if ( this.circleSearch == null )
+			this.circleSearch = new CircleSearchImpl();
+
+		return this.circleSearch;
+	}
+
+}
