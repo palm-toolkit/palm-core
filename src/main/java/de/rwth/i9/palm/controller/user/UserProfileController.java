6053678a79f2bd23768c95f3a47bef7a893d98b9
Sigit Nugraha <sigit.nugraha@gesis.org>
2016-01-13 01:02:52 +0100

    Update user profile and publication controllers

--- src/main/java/de/rwth/i9/palm/controller/user/UserProfileController.java
+++ src/main/java/de/rwth/i9/palm/controller/user/UserProfileController.java
@@ -1,12 +1,12 @@
 package de.rwth.i9.palm.controller.user;
 
-import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang3.text.WordUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,19 +19,19 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 import de.rwth.i9.palm.helper.TemplateHelper;
+import de.rwth.i9.palm.model.Author;
+import de.rwth.i9.palm.model.Institution;
 import de.rwth.i9.palm.model.SessionDataSet;
-import de.rwth.i9.palm.model.Source;
-import de.rwth.i9.palm.model.SourceProperty;
+import de.rwth.i9.palm.model.User;
 import de.rwth.i9.palm.model.Widget;
 import de.rwth.i9.palm.model.WidgetType;
 import de.rwth.i9.palm.persistence.PersistenceStrategy;
 import de.rwth.i9.palm.service.ApplicationContextService;
 import de.rwth.i9.palm.service.ApplicationService;
 import de.rwth.i9.palm.service.SecurityService;
-import de.rwth.i9.palm.wrapper.SourceListWrapper;
 
 @Controller
-@SessionAttributes( "sourceListWrapper" )
+@SessionAttributes( "user" )
 @RequestMapping( value = "/user/profile" )
 public class UserProfileController
 {
@@ -59,7 +59,9 @@ public class UserProfileController
 	 */
 	@Transactional
 	@RequestMapping( method = RequestMethod.GET )
-	public ModelAndView getSources( @RequestParam( value = "sessionid", required = false ) final String sessionId, final HttpServletResponse response) throws InterruptedException
+	public ModelAndView getSources( 
+			@RequestParam( value = "sessionid", required = false ) final String sessionId, 
+			final HttpServletResponse response) throws InterruptedException
 	{
 		// get current session object
 		SessionDataSet sessionDataSet = this.appService.getCurrentSessionDataSet();
@@ -70,7 +72,46 @@ public class UserProfileController
 
 		// assign the model
 		model.addObject( "widgets", widgets );
-		model.addObject( "user", securityService.getUser() );
+		User user = securityService.getUser();
+		model.addObject( "user", user );
+		if ( user.getAuthor() != null )
+		{
+			Author researcher = user.getAuthor();
+			/* get necessary author detail here */
+			Map<String, Object> researcherMap = new LinkedHashMap<String, Object>();
+			researcherMap.put( "id", researcher.getId() );
+			researcherMap.put( "name", WordUtils.capitalize( researcher.getName() ) );
+			if ( researcher.getPhotoUrl() != null )
+				researcherMap.put( "photo", researcher.getPhotoUrl() );
+			if ( researcher.getAcademicStatus() != null )
+				researcherMap.put( "status", researcher.getAcademicStatus() );
+			if ( researcher.getInstitutions() != null )
+				for ( Institution institution : researcher.getInstitutions() )
+				{
+					if ( researcherMap.get( "aff" ) != null )
+						researcherMap.put( "aff", researcherMap.get( "aff" ) + ", " + institution.getName() );
+					else
+						researcherMap.put( "aff", institution.getName() );
+				}
+			if ( researcher.getCitedBy() > 0 )
+				researcherMap.put( "citedBy", Integer.toString( researcher.getCitedBy() ) );
+
+			if ( researcher.getPublicationAuthors() != null )
+				researcherMap.put( "publicationsNumber", researcher.getPublicationAuthors().size() );
+			else
+				researcherMap.put( "publicationsNumber", 0 );
+			String otherDetail = "";
+			if ( researcher.getOtherDetail() != null )
+				otherDetail += researcher.getOtherDetail();
+			if ( researcher.getDepartment() != null )
+				otherDetail += ", " + researcher.getDepartment();
+			if ( !otherDetail.equals( "" ) )
+				researcherMap.put( "detail", otherDetail );
+
+			researcherMap.put( "isAdded", researcher.isAdded() );
+
+			model.addObject( "author", researcherMap );
+		}
 
 		return model;
 	}
@@ -86,43 +127,34 @@ public class UserProfileController
 	@Transactional
 	@RequestMapping( method = RequestMethod.POST )
 	public @ResponseBody Map<String, Object> saveSources( 
-			@ModelAttribute( "sourceListWrapper" ) SourceListWrapper sourceListWrapper, 
+			@ModelAttribute( "user" ) User user, 
+			@RequestParam( value = "userId", required = false ) final String userId, 
+			@RequestParam( value = "authorId", required = false ) final String authorId,
 			final HttpServletResponse response ) throws InterruptedException
 	{
-		// persist sources from model attribute
-		for ( Source source : sourceListWrapper.getSources() )
-		{
-			if ( source.getSourceProperties() != null || !source.getSourceProperties().isEmpty() )
-			{
-				Iterator<SourceProperty> iteratorSourceProperty = source.getSourceProperties().iterator();
-				while ( iteratorSourceProperty.hasNext() )
-				{
-					SourceProperty sourceProperty = iteratorSourceProperty.next();
-					// check for invalid sourceProperty object to be removed
-					if ( sourceProperty.getMainIdentifier().equals( "" ) || sourceProperty.getValue().equals( "" ) )
-					{
-						iteratorSourceProperty.remove();
-						// delete sourceProperty on database
-						persistenceStrategy.getSourcePropertyDAO().delete( sourceProperty );
-					}
-					else
-					{
-						sourceProperty.setSource( source );
-					}
-				}
-
-			}
-			persistenceStrategy.getSourceDAO().persist( source );
-		}
-
-		// set response
 		// create JSON mapper for response
 		Map<String, Object> responseMap = new LinkedHashMap<String, Object>();
-		responseMap.put( "status", "ok" );
-		responseMap.put( "format", "json" );
+				
+		// persist sources from model attribute
+		if( user == null && userId != null ){
+			user = persistenceStrategy.getUserDAO().getById( userId );
+		}
+		if( user == null){
+			responseMap.put( "status", "ok" );
+			return responseMap;
+		}
+		
+		Author author = null;
+		if ( authorId != null )
+			author = persistenceStrategy.getAuthorDAO().getById( authorId );
 
-		// update application service source cache
-		applicationService.updateAcademicNetworkSourcesCache();
+		if ( author != null )
+		{
+			user.setAuthor( author );
+			persistenceStrategy.getUserDAO().persist( user );
+		}
+		
+		responseMap.put( "status", "ok" );
 
 		return responseMap;
 	}
