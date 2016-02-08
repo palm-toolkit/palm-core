37b5f45ba49ec56b3e7dda66918145acf8291a2b
Sigit Nugraha <sigit.nugraha@gesis.org>
2016-02-08 20:42:44 +0100

    Update Manage Publication controller

--- src/main/java/de/rwth/i9/palm/controller/ManagePublicationController.java
+++ src/main/java/de/rwth/i9/palm/controller/ManagePublicationController.java
@@ -1,9 +1,12 @@
 package de.rwth.i9.palm.controller;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
@@ -25,7 +28,10 @@ import org.springframework.web.servlet.ModelAndView;
 import org.xml.sax.SAXException;
 
 import de.rwth.i9.palm.helper.TemplateHelper;
+import de.rwth.i9.palm.model.Author;
+import de.rwth.i9.palm.model.CompletionStatus;
 import de.rwth.i9.palm.model.Publication;
+import de.rwth.i9.palm.model.PublicationAuthor;
 import de.rwth.i9.palm.model.Widget;
 import de.rwth.i9.palm.model.WidgetType;
 import de.rwth.i9.palm.pdfextraction.service.PdfExtractionService;
@@ -93,11 +99,79 @@ public class ManagePublicationController
 	@Transactional
 	@RequestMapping( value = "/add", method = RequestMethod.POST )
 	public @ResponseBody Map<String, Object> saveNewPublication( 
-			@ModelAttribute( "publication" ) Publication publication, 
+			@ModelAttribute( "publication" ) Publication publication,
+			@RequestParam( value = "author-list-ids" , required= false ) String authorListIds,
+			@RequestParam( value = "keyword-list" , required= false ) String keywordList,
+			@RequestParam( value = "publication-date" , required= false ) String publicationDate,
+			@RequestParam( value = "venue-type" , required= false ) String venueType,
+			@RequestParam( value = "venue-id" , required= false ) String venueId,
+			@RequestParam( value = "issue" , required= false ) String issue,
+			@RequestParam( value = "pages" , required= false ) String pages,
+			@RequestParam( value = "publisher" , required= false ) String publisher,
 			final HttpServletResponse response) throws InterruptedException
 	{
-
 		Map<String, Object> responseMap = new LinkedHashMap<String, Object>();
+		if ( publication == null || publication.getTitle() == null || publication.getTitle().isEmpty() || authorListIds == null || authorListIds.isEmpty() )
+		{
+			responseMap.put( "status", "error" );
+			responseMap.put( "statusMessage", "Publication not found due to missing input or expired sission" );
+		}
+
+		/* Insert selected author into publication */
+		// get author id split by "_#_"
+		String[] authorIds = authorListIds.split( "_#_" );
+		int authorPosition = 0;
+		for ( String authorId : authorIds )
+		{
+			Author author = persistenceStrategy.getAuthorDAO().getById( authorId );
+			if ( author == null )
+				continue;
+
+			PublicationAuthor publicationAuthor = new PublicationAuthor();
+			publicationAuthor.setAuthor( author );
+			publicationAuthor.setPublication( publication );
+			publicationAuthor.setPosition( authorPosition );
+			publication.addPublicationAuthor( publicationAuthor );
+
+			authorPosition++;
+		}
+
+		if ( publication.getPublicationAuthors() == null || publication.getPublicationAuthors().isEmpty() )
+		{
+			responseMap.put( "status", "error" );
+			responseMap.put( "statusMessage", "Failed to save new publication, publication contain no authors" );
+		}
+
+		/* ABstract */
+		if ( publication.getAbstractText().isEmpty() )
+			publication.setAbstractText( null );
+		else
+		{
+			publication.setAbstractStatus( CompletionStatus.COMPLETE );
+		}
+
+		/* Insert Keyword if any */
+		if ( keywordList != null && !keywordList.isEmpty() )
+		{
+			publication.setKeywordStatus( CompletionStatus.COMPLETE );
+			publication.setKeywordText( keywordList.replace( "_#_", "," ) );
+		}
+
+		/* Insert publication date - expect valid publication date */
+		if ( publicationDate != null && !publicationDate.isEmpty() )
+		{
+			// set date format
+			DateFormat dateFormat = new SimpleDateFormat( "yyyy/M/d", Locale.ENGLISH );
+			try
+			{
+
+			}
+			catch ( Exception e )
+			{
+				// TODO: handle exception
+			}
+		}
+
 
 		return responseMap;
 	}
