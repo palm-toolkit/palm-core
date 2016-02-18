bba82ddcfbbc4df04af154beb7f21f8dd3074e8f
Sigit Nugraha <sigit.nugraha@gesis.org>
2016-02-18 01:24:30 +0100

    Add method to mining interest for circle on TopicExtractionService

--- src/main/java/de/rwth/i9/palm/topicextraction/service/TopicExtractionService.java
+++ src/main/java/de/rwth/i9/palm/topicextraction/service/TopicExtractionService.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import de.rwth.i9.palm.model.Author;
+import de.rwth.i9.palm.model.Circle;
 import de.rwth.i9.palm.model.ExtractionService;
 import de.rwth.i9.palm.model.ExtractionServiceType;
 import de.rwth.i9.palm.model.Publication;
@@ -157,6 +158,157 @@ public class TopicExtractionService
 
 		// save publications, set flag, prevent re-extract publication topic
 		for ( Publication publication : author.getPublications() )
+		{
+			publication.setContentUpdated( false );
+//			System.out.println( publication.getId() + " > " + publication.getTitle() );
+//			Set<PublicationTopic> topics = publication.getPublicationTopics();
+//
+//			for ( PublicationTopic topic : topics )
+//			{
+//				if ( topic.getExtractionServiceType().equals( ExtractionServiceType.ALCHEMYAPI ) )
+//					if ( topic.getTermValues() != null )
+//					{
+//					System.out.println( topic.getExtractionServiceType().toString() + " : " );
+//						System.out.print( topic.getId() + " > " );
+//						for ( Entry<String, Double> termValue : topic.getTermValues().entrySet() )
+//							System.out.print( termValue.getKey() + " : " + termValue.getValue() + " | " );
+//					System.out.println();
+//					}
+//			}
+//			System.out.println();
+
+			persistenceStrategy.getPublicationDAO().persist( publication );
+		}
+	}
+	
+	
+	public void extractTopicFromPublicationByCircle( Circle circle ) throws InterruptedException, UnsupportedEncodingException, URISyntaxException
+	{
+		List<Future<PublicationTopic>> publicationTopicFutureList = new ArrayList<Future<PublicationTopic>>();
+		List<ExtractionService> extractionServices = persistenceStrategy.getExtractionServiceDAO().getAllActiveExtractionService();
+		
+		// get current date
+		Calendar calendar = Calendar.getInstance();
+
+		boolean topicExtractionUpdated = false;
+
+		// loop through available extraction services
+		for ( ExtractionService extractionService : extractionServices )
+		{
+			if ( !extractionService.isActive() )
+				continue;
+
+			// check extraction service limitation (number of queries per day)
+			// TODO this is still not correct
+			if ( extractionService.getLastQueryDate() != null )
+			{
+				if ( extractionService.getLastQueryDate().equals( calendar.getTime() ) )
+				{
+					extractionService.setCountQueryThisDay( extractionService.getCountQueryThisDay() + circle.getPublications().size() );
+					persistenceStrategy.getExtractionServiceDAO().persist( extractionService );
+				}
+				else
+				{
+					extractionService.setLastQueryDate( calendar.getTime() );
+					extractionService.setCountQueryThisDay( circle.getPublications().size() );
+					persistenceStrategy.getExtractionServiceDAO().persist( extractionService );
+				}
+			}
+			else
+			{
+				extractionService.setLastQueryDate( calendar.getTime() );
+				extractionService.setCountQueryThisDay( circle.getPublications().size() );
+				persistenceStrategy.getExtractionServiceDAO().persist( extractionService );
+			}
+
+			// if beyond limitation query perday
+			if ( extractionService.getCountQueryThisDay() > extractionService.getMaxQueryPerDay() )
+				continue;
+			
+			// publications on specific user
+			for ( Publication publication : circle.getPublications() )
+			{
+				if ( publication.getAbstractText() == null )
+					continue;
+
+				// at least have an abstract
+				if ( publication.isContentUpdated() )
+				{
+					// // remove old extracted source
+					// if ( publication.getPublicationTopics() != null )
+					// publication.removeAllPublicationTopic();
+
+					// create new publication topic
+					PublicationTopic publicationTopic = new PublicationTopic();
+					publicationTopic.setExtractionServiceType( extractionService.getExtractionServiceType() );
+					publicationTopic.setExtractionDate( calendar.getTime() );
+					publicationTopic.setPublication( publication );
+
+					publication.addPublicationTopic( publicationTopic );
+
+					if ( extractionService.getExtractionServiceType().equals( ExtractionServiceType.ALCHEMY ) )
+						publicationTopicFutureList.add( asynchronousTopicExtractionService.getTopicsByAlchemyApi( publication, publicationTopic, extractionService.getMaxTextLength() ) );
+					else if ( extractionService.getExtractionServiceType().equals( ExtractionServiceType.YAHOOCONTENTANALYSIS ) )
+						publicationTopicFutureList.add( asynchronousTopicExtractionService.getTopicsByYahooContentAnalysis( publication, publicationTopic, extractionService.getMaxTextLength() ) );
+					else if ( extractionService.getExtractionServiceType().equals( ExtractionServiceType.FIVEFILTERS ) )
+						publicationTopicFutureList.add( asynchronousTopicExtractionService.getTopicsByFiveFilters( publication, publicationTopic, extractionService.getMaxTextLength() ) );
+				}
+				else
+				{
+					// if something fails on last run
+					PublicationTopic publicationTopic = null;
+					for ( PublicationTopic publicationTopicEach : publication.getPublicationTopics() )
+					{
+						if ( publicationTopicEach.getExtractionServiceType().equals( extractionService.getExtractionServiceType() ) )
+						{
+							publicationTopic = publicationTopicEach;
+						}
+					}
+
+					if ( publicationTopic == null )
+					{
+						publicationTopic = new PublicationTopic();
+						publicationTopic.setExtractionServiceType( extractionService.getExtractionServiceType() );
+						publicationTopic.setExtractionDate( calendar.getTime() );
+						publicationTopic.setPublication( publication );
+
+						publication.addPublicationTopic( publicationTopic );
+					}
+					if ( publicationTopic.getTermValues() == null || publicationTopic.getTermValues().isEmpty() )
+					{
+						if ( extractionService.getExtractionServiceType().equals( ExtractionServiceType.ALCHEMY ) )
+							publicationTopicFutureList.add( asynchronousTopicExtractionService.getTopicsByAlchemyApi( publication, publicationTopic, extractionService.getMaxTextLength() ) );
+						else if ( extractionService.getExtractionServiceType().equals( ExtractionServiceType.YAHOOCONTENTANALYSIS ) )
+							publicationTopicFutureList.add( asynchronousTopicExtractionService.getTopicsByYahooContentAnalysis( publication, publicationTopic, extractionService.getMaxTextLength() ) );
+						else if ( extractionService.getExtractionServiceType().equals( ExtractionServiceType.FIVEFILTERS ) )
+							publicationTopicFutureList.add( asynchronousTopicExtractionService.getTopicsByFiveFilters( publication, publicationTopic, extractionService.getMaxTextLength() ) );
+					}
+
+				}
+				
+			}
+
+		}
+		// check whether thread worker is done
+		// Wait until they are all done
+		boolean processIsDone = true;
+		do
+		{
+			processIsDone = true;
+			for ( Future<PublicationTopic> futureList : publicationTopicFutureList )
+			{
+				if ( !futureList.isDone() )
+				{
+					processIsDone = false;
+					break;
+				}
+			}
+			// 10-millisecond pause between each check
+			Thread.sleep( 10 );
+		} while ( !processIsDone );
+
+		// save publications, set flag, prevent re-extract publication topic
+		for ( Publication publication : circle.getPublications() )
 		{
 			publication.setContentUpdated( false );
 //			System.out.println( publication.getId() + " > " + publication.getTitle() );
