2fcc43bcd020979fc79c41df9296e8936df0e45c
Sigit Nugraha <sigit.nugraha@gesis.org>
2016-04-29 13:55:19 +0200

    Ignore DBLP collection

--- src/test/java/de/rwth/i9/palm/datasetcollect/service/DblpPublicationCollectionTest.java
+++ src/test/java/de/rwth/i9/palm/datasetcollect/service/DblpPublicationCollectionTest.java
@@ -17,6 +17,7 @@ import de.rwth.i9.palm.model.SourceProperty;
 
 @RunWith( SpringJUnit4ClassRunner.class )
 @ContextConfiguration( loader = AnnotationConfigContextLoader.class )
+@Ignore
 public class DblpPublicationCollectionTest
 {
 	@Test
@@ -67,7 +68,7 @@ public class DblpPublicationCollectionTest
 		source.addSourceProperty( sourceProperty1 );
 		source.addSourceProperty( sourceProperty2 );
 
-		List<Map<String, String>> publicationMapLists = DblpPublicationCollection.getPublicationListByAuthorUrl( "http://dblp.uni-trier.de/pers/hd/w/Wira=Alam:Andias", source );
+		List<Map<String, String>> publicationMapLists = DblpPublicationCollection.getPublicationListByAuthorUrl( "http://dblp.uni-trier.de/pers/hd/k/Krau=szlig=:Christopher", source );
 		int count = 1;
 
 		for ( Map<String, String> eachPublicationMap : publicationMapLists )
