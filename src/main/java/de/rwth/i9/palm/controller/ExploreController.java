1932014a378c13bcff8f2d066838ffcb6d8706ca
Manpriya <manpriya.guliani@gmail.com>
2016-05-26 11:56:33 +0200

    show researcher data in va screen

--- src/main/java/de/rwth/i9/palm/controller/ExploreController.java
+++ src/main/java/de/rwth/i9/palm/controller/ExploreController.java
@@ -1,14 +1,19 @@
 package de.rwth.i9.palm.controller;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URISyntaxException;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.oltu.oauth2.common.exception.OAuthProblemException;
+import org.apache.oltu.oauth2.common.exception.OAuthSystemException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,7 +24,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import de.rwth.i9.palm.feature.publication.PublicationFeature;
+import de.rwth.i9.palm.feature.researcher.ResearcherFeature;
 import de.rwth.i9.palm.helper.TemplateHelper;
+import de.rwth.i9.palm.model.Author;
+import de.rwth.i9.palm.model.AuthorSource;
 import de.rwth.i9.palm.model.User;
 import de.rwth.i9.palm.model.UserWidget;
 import de.rwth.i9.palm.model.Widget;
@@ -32,8 +40,7 @@ import de.rwth.i9.palm.service.SecurityService;
 @RequestMapping( value = "/explore" )
 public class ExploreController
 {
-	// private final static Logger log = LoggerFactory.getLogger(
-	// ExploreController.class );
+	private final static Logger log = LoggerFactory.getLogger( ExploreController.class );
 
 	private static final String LINK_NAME = "explore";
 
@@ -45,10 +52,10 @@ public class ExploreController
 
 	@Autowired
 	private PublicationFeature publicationFeature;
-	//
-	// @Autowired
-	// private ResearcherFeature researcherFeature;
-	//
+
+	@Autowired
+	private ResearcherFeature researcherFeature;
+
 	// @Autowired
 	// private PublicationCollectionService publicationCollectionService;
 
@@ -97,12 +104,98 @@ public class ExploreController
 		return model;
 	}
 
-	@RequestMapping( value = "/topic", method = RequestMethod.GET )
+	@SuppressWarnings( "unchecked" )
 	@Transactional
-	public @ResponseBody Map<String, Object> getPublicationTopic( @RequestParam( value = "id", required = false ) final String id, @RequestParam( value = "pid", required = false ) final String pid, @RequestParam( value = "maxRetrieve", required = false ) final String maxRetrieve, final HttpServletResponse response ) throws UnsupportedEncodingException, InterruptedException, URISyntaxException, ExecutionException
+	@RequestMapping( value = "/researchers", method = RequestMethod.GET )
+	public @ResponseBody Map<String, Object> getAuthorList( @RequestParam( value = "query", required = false ) String query, @RequestParam( value = "queryType", required = false ) String queryType, @RequestParam( value = "page", required = false ) Integer startPage, @RequestParam( value = "maxresult", required = false ) Integer maxresult, @RequestParam( value = "source", required = false ) String source, @RequestParam( value = "addedAuthor", required = false ) String addedAuthor, @RequestParam( value = "fulltextSearch", required = false ) String fulltextSearch, @RequestParam( value = "persist", required = false ) String persist, HttpServletRequest request, HttpServletResponse response ) throws IOException, InterruptedException, ExecutionException, org.apache.http.ParseException, OAuthSystemException, OAuthProblemException
 	{
-		System.out.println( "comes HERE!!!!" );
-		return publicationFeature.getPublicationMining().getPublicationExtractedTopicsById( "45998734-75d0-4eef-89bc-e69b109cfdfd", pid, maxRetrieve );
+
+		/* == Set Default Values== */
+		if ( query == null )
+			query = "";
+		if ( queryType == null )
+			queryType = "name";
+		if ( startPage == null )
+			startPage = 0;
+		if ( maxresult == null )
+			maxresult = 50;
+		if ( source == null )
+			source = "internal";
+		if ( addedAuthor == null )
+			addedAuthor = "no";
+		if ( fulltextSearch == null )
+			fulltextSearch = "no";
+		if ( persist == null )
+			persist = "no";
+
+		// create JSON mapper for response
+		Map<String, Object> responseMap = new LinkedHashMap<String, Object>();
+		boolean persistResult = false;
+
+		responseMap.put( "query", query );
+		if ( !queryType.equals( "name" ) )
+			responseMap.put( "queryType", queryType );
+		responseMap.put( "page", startPage );
+		responseMap.put( "maxresult", maxresult );
+		responseMap.put( "source", source );
+		if ( !fulltextSearch.equals( "no" ) )
+			responseMap.put( "fulltextSearch", fulltextSearch );
+		if ( !persist.equals( "no" ) )
+		{
+			responseMap.put( "persist", persist );
+			persistResult = true;
+		}
+		if ( addedAuthor.equals( "yes" ) )
+			responseMap.put( "addedAuthor", addedAuthor );
+
+		Map<String, Object> authorsMap = researcherFeature.getResearcherSearch().getResearcherMapByQuery( query, queryType, startPage, maxresult, source, addedAuthor, fulltextSearch, persistResult );
+
+		// store in session
+		if ( source.equals( "external" ) || source.equals( "all" ) )
+		{
+			request.getSession().setAttribute( "researchers", authorsMap.get( "authors" ) );
+
+			// recheck if session really has been updated
+			// (there is a bug in spring session, which makes session is
+			// not updated sometimes) - a little workaround
+			boolean isSessionUpdated = false;
+			while ( !isSessionUpdated )
+			{
+				Object authors = request.getSession().getAttribute( "researchers" );
+				if ( authors.equals( authorsMap.get( "authors" ) ) )
+					isSessionUpdated = true;
+				else
+					request.getSession().setAttribute( "researchers", authorsMap.get( "authors" ) );
+			}
+
+			log.info( "\nRESEARCHER SESSION SEARCH" );
+			@SuppressWarnings( "unchecked" )
+			List<Author> sessionAuthors = (List<Author>) request.getSession().getAttribute( "researchers" );
+			// get author from session -> just for debug
+			if ( sessionAuthors != null && !sessionAuthors.isEmpty() )
+			{
+				for ( Author sessionAuthor : sessionAuthors )
+				{
+					for ( AuthorSource as : sessionAuthor.getAuthorSources() )
+					{
+						log.info( sessionAuthor.getId() + "-" + sessionAuthor.getName() + " - " + as.getSourceType() + " -> " + as.getSourceUrl() );
+					}
+				}
+			}
+
+		}
+
+		if ( authorsMap != null && (Integer) authorsMap.get( "totalCount" ) > 0 )
+		{
+			responseMap.put( "totalCount", (Integer) authorsMap.get( "totalCount" ) );
+			return researcherFeature.getResearcherSearch().printJsonOutput( responseMap, (List<Author>) authorsMap.get( "authors" ) );
+		}
+		else
+		{
+			responseMap.put( "totalCount", 0 );
+			responseMap.put( "count", 0 );
+			return responseMap;
+		}
 	}
 
 }
