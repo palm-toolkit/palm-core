0c898c0338c89a312e331260d331323ad1eb00ee
Manpriya <manpriya.guliani@gmail.com>
2016-06-11 13:13:00 +0200

    Revert "Updates on Test GetDataAndMahout & TestGetDataCircle"
    This reverts commit c0d06e90b9aaa1cbc0938d3290d1f418224e8fd6.

--- src/test/java/de/rwth/i9/palm/test/TestGetDataCircle.java
+++ src/test/java/de/rwth/i9/palm/test/TestGetDataCircle.java
@@ -5,7 +5,6 @@ import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.List;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -47,7 +46,6 @@ public class TestGetDataCircle extends AbstractTransactionalJUnit4SpringContextT
 
 
 	@Test
-	@Ignore
 	public void testGetCirclePublicationsFromDatabase() throws FileNotFoundException, UnsupportedEncodingException
 	{
 		int count = 0;
@@ -78,42 +76,5 @@ public class TestGetDataCircle extends AbstractTransactionalJUnit4SpringContextT
 				count = 0;
 				writer.close();
 		}
-	}
-
-	@Test
-	public void testGetCirclePublicationsFromDatabaseYearly() throws FileNotFoundException, UnsupportedEncodingException
-	{
-		int count = 0;
-		System.out.println( "\n========== TEST 1 - Get Circle publication ==========" );
-		// Circle circle = persistenceStrategy.getCircleDAO().getById(
-		// "e61d08f9-afd4-4600-9c16-78a62cdfbee0" );
-
-		List<Circle> circles = persistenceStrategy.getCircleDAO().getAll();
-
-		if ( !circles.isEmpty() )
-			for ( Circle circle : circles )
-
-			{
-				for ( int year = 1980; year < 2017; year++ )
-				{
-					PrintWriter writer = new PrintWriter( "C:/Users/Piro/Desktop/Circles/Circles/" + circle.getId() + ".txt", "UTF-8" );
-					writer.println( "Circle Name : " + circle.getName() );
-					for ( Publication publication : circle.getPublications() )
-					{
-						if ( publication.getAbstractText() != null )
-						{
-							writer.println( publication.getTitle() );
-							writer.println( publication.getAbstractText() );
-							writer.println();
-							count++;
-						}
-					}
-
-				writer.println();
-				writer.println( count );
-				count = 0;
-				writer.close();
-				}
-			}
 	}
 }
