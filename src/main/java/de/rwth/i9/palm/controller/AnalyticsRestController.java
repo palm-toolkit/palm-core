220cbd6064eab674828a5fee04cba55cb6c8ab29
Jan <jan@23x5.net>
2016-06-13 23:02:11 +0200

    Initial commit

---
+++ src/main/java/de/rwth/i9/palm/controller/AnalyticsRestController.java
@@ -0,0 +1,183 @@
+package de.rwth.i9.palm.controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import de.rwth.i9.palm.analytics.algorithm.clustering.WekaSimpleKMeans;
+import de.rwth.i9.palm.analytics.api.PalmAnalytics;
+import de.rwth.i9.palm.feature.researcher.ResearcherFeature;
+import de.rwth.i9.palm.interestmining.service.InterestMiningService;
+import de.rwth.i9.palm.model.Author;
+import de.rwth.i9.palm.model.Publication;
+import de.rwth.i9.palm.persistence.PersistenceStrategy;
+import weka.clusterers.SimpleKMeans;
+import weka.core.Attribute;
+import weka.core.DenseInstance;
+import weka.core.Instance;
+import weka.core.Instances;
+
+@RestController
+@RequestMapping( value = "/analytics" )
+public class AnalyticsRestController
+{
+	@Autowired
+	private PalmAnalytics palmAnalytics;
+
+	@Autowired
+	private PersistenceStrategy persistenceStrategy;
+
+	@Autowired
+	private ResearcherFeature researcherFeature;
+
+	@Autowired
+	private InterestMiningService interestMiningService;
+
+	private ObjectMapper mapper = new ObjectMapper();
+
+	@RequestMapping( "/clustering/{type}" )
+	@Transactional
+	public String simpleKMeans( @PathVariable("type") String objectType,
+			@RequestParam( value = "algorithm", defaultValue = "kmeans" ) String algorithm, @RequestParam( value = "relatedObjectType", required = false ) String relatedObjectType, @RequestParam( value = "relatedObjectId", required = false ) String relatedObjectId
+			) throws Exception
+	{	
+		Map<String, Integer> resultMap;
+
+		switch ( objectType ) {
+		case "authors":
+			resultMap = clusterAuthors( algorithm, relatedObjectId, relatedObjectType );
+			break;
+		case "publications":
+			resultMap = clusterPublications( algorithm, relatedObjectId, relatedObjectType );
+			break;
+		default:
+			resultMap = clusterAuthors( algorithm, relatedObjectId, relatedObjectType);
+			break;
+		}
+
+		return mapper.writeValueAsString( resultMap );
+	}
+
+	private Map<String, Integer> clusterPublications( String algorithm, String relatedObjectId, String relatedObjectType ) throws Exception
+	{
+		Map<String, Integer> resultMap = new HashMap<String, Integer>();
+
+		List<Publication> publications;
+		if ( relatedObjectId != null && relatedObjectType != null )
+		{
+			switch ( relatedObjectType ) {
+			case "event":
+				publications = persistenceStrategy.getPublicationDAO().getPublicationByEventId( relatedObjectId );
+				break;
+			case "author":
+				publications = persistenceStrategy.getPublicationDAO().getPublicationByAuthorId( relatedObjectId );
+				break;
+			default:
+				publications = persistenceStrategy.getPublicationDAO().getPublicationByEventId( relatedObjectId );
+				break;
+			}
+		}
+		else
+		{
+			publications = persistenceStrategy.getPublicationDAO().getAll();
+		}
+
+		if ( publications.size() > 0 )
+		{
+			System.out.println( "Clustering Publications" );
+			ArrayList<Attribute> attributes = new ArrayList<Attribute>();
+			Attribute citedBy = new Attribute( "citedBy" );
+			attributes.add( citedBy );
+
+			Instances data = new Instances( "publications", attributes, publications.size() );
+
+			for ( Publication p : publications )
+			{
+				Instance i = new DenseInstance( attributes.size() );
+				i.setValue( citedBy, p.getCitedBy() );
+				data.add( i );
+			}
+
+			if ( algorithm.equals( "kmeans" ) )
+			{
+				SimpleKMeans result = WekaSimpleKMeans.run( 4, data );
+				int i = 0;
+				for ( int clusterNum : result.getAssignments() )
+				{
+					resultMap.put( mapper.writeValueAsString( publications.get( i ).getJsonStub() ), clusterNum );
+					i++;
+				}
+			}
+		}
+
+		return resultMap;
+	}
+
+	private Map<String, Integer> clusterAuthors( String algorithm, String relatedObjectId, String relatedObjectType ) throws Exception
+	{
+		Map<String, Integer> resultMap = new HashMap<String, Integer>();
+
+		List<Author> authors;
+		System.out.println( "Clustering Authors" );
+		if ( relatedObjectId != null && relatedObjectType != null )
+		{
+			switch ( relatedObjectType ) {
+			case "event":
+				authors = persistenceStrategy.getAuthorDAO().getAuthorByEventId( relatedObjectId );
+				break;
+			case "publication":
+				Publication publication = persistenceStrategy.getPublicationDAO().getById( relatedObjectId );
+				authors = publication.getAuthors();
+				break;
+			default:
+				authors = persistenceStrategy.getAuthorDAO().getAuthorByEventId( relatedObjectId );
+			}
+		}
+		else
+		{
+			authors = persistenceStrategy.getAuthorDAO().getAdded();
+		}
+
+		if ( authors.size() > 0 )
+		{
+			ArrayList<Attribute> attributes = new ArrayList<Attribute>();
+			Attribute citedBy = new Attribute( "citedBy" );
+			attributes.add( citedBy );
+			Attribute noPublication = new Attribute( "noPublication" );
+			attributes.add( noPublication );
+
+			Instances data = new Instances( "authors", attributes, authors.size() );
+
+			for ( Author a : authors )
+			{
+				Instance i = new DenseInstance( attributes.size() );
+				i.setValue( citedBy, a.getCitedBy() );
+				i.setValue( noPublication, a.getNoPublication() );
+				data.add( i );
+			}
+
+			if ( algorithm.equals( "kmeans" ) )
+			{
+				SimpleKMeans result = WekaSimpleKMeans.run( 4, data );
+				int i = 0;
+				for ( int clusterNum : result.getAssignments() )
+				{
+					resultMap.put( mapper.writeValueAsString( authors.get( i ).getJsonStub() ), clusterNum );
+					i++;
+				}
+			}
+		}
+
+		return resultMap;
+	}
+}
