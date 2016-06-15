7d343bd4a8a4336c8ee34b54668ee82cfee66fa8
Jan <jan@23x5.net>
2016-06-16 00:23:35 +0200

    ...

--- src/main/java/de/rwth/i9/palm/controller/AnalyticsRestController.java
+++ src/main/java/de/rwth/i9/palm/controller/AnalyticsRestController.java
@@ -1,9 +1,6 @@
 package de.rwth.i9.palm.controller;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,19 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import de.rwth.i9.palm.analytics.algorithm.clustering.WekaSimpleKMeans;
 import de.rwth.i9.palm.analytics.api.PalmAnalytics;
-import de.rwth.i9.palm.feature.researcher.ResearcherFeature;
 import de.rwth.i9.palm.interestmining.service.InterestMiningService;
-import de.rwth.i9.palm.model.Author;
+import de.rwth.i9.palm.model.DataMiningAuthor;
 import de.rwth.i9.palm.model.DataMiningPublication;
-import de.rwth.i9.palm.model.Publication;
 import de.rwth.i9.palm.persistence.PersistenceStrategy;
-import weka.clusterers.SimpleKMeans;
-import weka.core.Attribute;
-import weka.core.DenseInstance;
-import weka.core.Instance;
-import weka.core.Instances;
 
 @RestController
 @RequestMapping( value = "/analytics" )
@@ -40,30 +29,28 @@ public class AnalyticsRestController
 	private PersistenceStrategy persistenceStrategy;
 
 	@Autowired
-	private ResearcherFeature researcherFeature;
-
-	@Autowired
 	private InterestMiningService interestMiningService;
 
 	private ObjectMapper mapper = new ObjectMapper();
 
+
 	@RequestMapping( "/clustering/{type}" )
 	@Transactional
 	public String simpleKMeans( @PathVariable("type") String objectType,
-			@RequestParam( value = "algorithm", defaultValue = "kmeans" ) String algorithm, @RequestParam( value = "relatedObjectType", required = false ) String relatedObjectType, @RequestParam( value = "relatedObjectId", required = false ) String relatedObjectId
+			@RequestParam( value = "algorithm", defaultValue = "kmeans" ) String algorithm, @RequestParam( value = "relatedObjectType", required = false ) String relatedObjectType, @RequestParam( value = "relatedObjectId", required = false ) String relatedObjectId, @RequestParam( value = "algorithmParameters", defaultValue = "4" ) String algorithmParameters
 			) throws Exception
 	{	
 		Map<String, Integer> resultMap;
 
 		switch ( objectType ) {
 		case "authors":
-			resultMap = clusterAuthors( algorithm, relatedObjectId, relatedObjectType );
+			resultMap = clusterAuthors( algorithm, relatedObjectId, relatedObjectType, algorithmParameters );
 			break;
 		case "publications":
 			resultMap = clusterPublications( algorithm, relatedObjectId, relatedObjectType );
 			break;
 		default:
-			resultMap = clusterAuthors( algorithm, relatedObjectId, relatedObjectType);
+			resultMap = clusterAuthors( algorithm, relatedObjectId, relatedObjectType, algorithmParameters );
 			break;
 		}
 
@@ -75,69 +62,19 @@ public class AnalyticsRestController
 		Map<DataMiningPublication, Integer> clustering = palmAnalytics.getClustering().clusterPublications( persistenceStrategy, algorithm, relatedObjectId, relatedObjectType );
 		Map<String, Integer> resultMap = new HashMap<String, Integer>();
 		for(DataMiningPublication p : clustering.keySet()) {
-			System.out.println( p.getTitle() + ", " + p.getId() );
 			resultMap.put( mapper.writeValueAsString( p.getJsonStub() ), clustering.get( p ) );
 		}
 		return resultMap;
 	}
 
-	private Map<String, Integer> clusterAuthors( String algorithm, String relatedObjectId, String relatedObjectType ) throws Exception
+	private Map<String, Integer> clusterAuthors( String algorithm, String relatedObjectId, String relatedObjectType, String algorithmParameters ) throws Exception
 	{
+		Map<DataMiningAuthor, Integer> clustering = palmAnalytics.getClustering().clusterAuthors( persistenceStrategy, algorithm, relatedObjectId, relatedObjectType, algorithmParameters );
 		Map<String, Integer> resultMap = new HashMap<String, Integer>();
-
-		List<Author> authors;
-		System.out.println( "Clustering Authors" );
-		if ( relatedObjectId != null && relatedObjectType != null )
+		for ( DataMiningAuthor a : clustering.keySet() )
 		{
-			switch ( relatedObjectType ) {
-			case "event":
-				authors = persistenceStrategy.getAuthorDAO().getAuthorByEventId( relatedObjectId );
-				break;
-			case "publication":
-				Publication publication = persistenceStrategy.getPublicationDAO().getById( relatedObjectId );
-				authors = publication.getAuthors();
-				break;
-			default:
-				authors = persistenceStrategy.getAuthorDAO().getAuthorByEventId( relatedObjectId );
-			}
+			resultMap.put( mapper.writeValueAsString( a.getJsonStub() ), clustering.get( a ) );
 		}
-		else
-		{
-			authors = persistenceStrategy.getAuthorDAO().getAdded();
-		}
-
-		if ( authors.size() > 0 )
-		{
-			ArrayList<Attribute> attributes = new ArrayList<Attribute>();
-			Attribute citedBy = new Attribute( "citedBy" );
-			attributes.add( citedBy );
-			Attribute noPublication = new Attribute( "noPublication" );
-			attributes.add( noPublication );
-
-			Instances data = new Instances( "authors", attributes, authors.size() );
-
-			for ( Author a : authors )
-			{
-				Instance i = new DenseInstance( attributes.size() );
-				i.setValue( citedBy, a.getCitedBy() );
-				i.setValue( noPublication, a.getNoPublication() );
-				data.add( i );
-				Map<String, Object> m = new LinkedHashMap<String, Object>();
-				interestMiningService.getInterestFromDatabase( a, m );
-			}
-
-			if ( algorithm.equals( "kmeans" ) )
-			{
-				SimpleKMeans result = WekaSimpleKMeans.run( 4, data );
-				int i = 0;
-				for ( int clusterNum : result.getAssignments() )
-				{
-					resultMap.put( mapper.writeValueAsString( authors.get( i ).getJsonStub() ), clusterNum );
-					i++;
-				}
-			}
-		}
-
 		return resultMap;
 
 	}
--- src/main/java/de/rwth/i9/palm/controller/DataController.java
+++ src/main/java/de/rwth/i9/palm/controller/DataController.java
@@ -1,6 +1,10 @@
 package de.rwth.i9.palm.controller;
 
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +13,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import de.rwth.i9.palm.model.Author;
+import de.rwth.i9.palm.model.AuthorInterest;
+import de.rwth.i9.palm.model.AuthorInterestFlat;
+import de.rwth.i9.palm.model.AuthorInterestProfile;
+import de.rwth.i9.palm.model.DataMiningPublication;
+import de.rwth.i9.palm.model.Interest;
+import de.rwth.i9.palm.model.PublicationTopic;
+import de.rwth.i9.palm.model.PublicationTopicFlat;
 import de.rwth.i9.palm.persistence.PersistenceStrategy;
 
 @Controller
@@ -113,5 +125,72 @@ public class DataController
 			return ( e.getMessage() );
 		}
 		return "re indexing circle complete";
+	}
+
+	@Transactional
+	@RequestMapping( value = "/updateflattables/authors", method = RequestMethod.GET )
+	public @ResponseBody String updateFlatAuthor()
+	{
+		List<Author> authors = persistenceStrategy.getAuthorDAO().getAdded();
+		int j = 1;
+		for ( Author author : authors )
+		{
+			if ( persistenceStrategy.getAuthorInterestFlatDAO().authorIdExists( author.getId() ) )
+				continue;
+			Map<String, Double> authorInterests = new HashMap<String, Double>();
+			for ( AuthorInterestProfile aip : author.getAuthorInterestProfiles() )
+			{
+				for ( AuthorInterest ai : aip.getAuthorInterests() )
+				{
+					Map<Interest, Double> termWeights = ai.getTermWeights();
+					for ( Interest i : termWeights.keySet() )
+					{
+						Double value = authorInterests.containsKey( i.getTerm() ) ? authorInterests.get( i.getTerm() ) : 0;
+						authorInterests.put( i.getTerm(), value + termWeights.get( i ) );
+					}
+				}
+			}
+			AuthorInterestFlat aif = new AuthorInterestFlat();
+			aif.setAuthor_id( author.getId() );
+			aif.setInterests( authorInterests.toString().replaceAll( "[\\{\\}]", "" ) );
+			persistenceStrategy.getAuthorInterestFlatDAO().persist( aif );
+			System.out.println( "Added no " + j );
+			j++;
+		}
+
+		return "Updated flat author table.";
+	}
+
+	@Transactional
+	@RequestMapping( value = "/updateflattables/publications", method = RequestMethod.GET )
+	public @ResponseBody String updateFlatPublication()
+	{
+		List<DataMiningPublication> publications = persistenceStrategy.getPublicationDAO().getDataMiningObjects();
+		int j = 1;
+		for ( DataMiningPublication publication : publications )
+		{
+			// if (
+			// persistenceStrategy.getPublicationTopicFlatDAO().publicationIdExists(
+			// publication.getId() ) )
+			// continue;
+			Map<String, Double> publicationTopics = new HashMap<String, Double>();
+			for ( PublicationTopic pt : publication.getPublicationTopics() )
+			{
+				for ( String term : pt.getTermValues().keySet() )
+				{
+					Double value = publicationTopics.containsKey( term ) ? publicationTopics.get( term ) : 0;
+					publicationTopics.put( term, value + pt.getTermValues().get( term ) );
+				}
+			}
+			PublicationTopicFlat ptf = new PublicationTopicFlat();
+			ptf.setPublication_id( publication.getId() );
+			System.out.println( publicationTopics.toString().replaceAll( "[\\{\\}]", "" ) );
+			ptf.setTopics( publicationTopics.toString().replaceAll( "[\\{\\}]", "" ) );
+			persistenceStrategy.getPublicationTopicFlatDAO().persist( ptf );
+			System.out.println( "Added no " + j );
+			j++;
+		}
+
+		return "Updated flat publication tables";
 	}
 }
