581170eff6fc44f482a169b3b38157a7c8733193
Piro Lena <piro_lena@hotmail.com>
2016-06-24 02:11:30 +0200

    Updates on Researchers - Topic Level Similarity, Similarity and TagCloud

--- src/main/java/de/rwth/i9/palm/feature/researcher/ResearcherSimilarauthorImpl.java
+++ src/main/java/de/rwth/i9/palm/feature/researcher/ResearcherSimilarauthorImpl.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -81,6 +82,76 @@ public class ResearcherSimilarauthorImpl implements ResearcherSimilarauthor
 		}
 
 		// remove unnecessary result
+
+		// put similarAuthor to responseMap
+		responseMap.put( "countTotal", similarAuthorList.size() );
+		responseMap.put( "count", similarAuthorListPaging.size() );
+		responseMap.put( "similarAuthors", similarAuthorListPaging );
+
+		return responseMap;
+	}
+
+	@Override
+	public Map<String, Object> getResearcherSimilarAuthorTopicLevelMap( Author author, int startPage, int maxresult )
+	{
+		// researchers list container
+		Map<String, Object> responseMap = new LinkedHashMap<String, Object>();
+
+		Map<String, List<String>> similarAuthors = new LinkedHashMap<String, List<String>>();
+		similarAuthors = palmAnalytics.getNGrams().runSimilarEntitiesTopicLevel( author.getId().toString(), "C:/Users/Piro/Desktop/", "Authors", 30, 10, 3, false );
+
+		// get the id, degree of similarity, topics proportions
+		// put them into the map
+		// prepare list of object map containing similarAuthor details
+		List<Map<String, Object>> similarAuthorList = new ArrayList<Map<String, Object>>();
+
+		for ( Entry<String, List<String>> similar : similarAuthors.entrySet() )
+		{
+			if ( !persistenceStrategy.getAuthorDAO().getById( similar.getKey().split( "->" )[0] ).equals( author ) )
+			{
+			Map<String, Object> similarAuthorMap = new LinkedHashMap<String, Object>();
+
+			// insert the initial basic information
+			similarAuthorMap.put( "id", similar.getKey().split( "->" )[0] );
+			similarAuthorMap.put( "name", persistenceStrategy.getAuthorDAO().getById( similar.getKey().split( "->" )[0] ).getName() );
+			if ( persistenceStrategy.getAuthorDAO().getById( similar.getKey().split( "->" )[0] ).getInstitution() != null )
+				similarAuthorMap.put( "affiliation", persistenceStrategy.getAuthorDAO().getById( similar.getKey().split( "->" )[0] ).getInstitution().getName() );
+			if ( persistenceStrategy.getAuthorDAO().getById( similar.getKey().split( "->" )[0] ).getPhotoUrl() != null )
+				similarAuthorMap.put( "photo", persistenceStrategy.getAuthorDAO().getById( similar.getKey().split( "->" )[0] ).getPhotoUrl() );
+			similarAuthorMap.put( "isAdded", persistenceStrategy.getAuthorDAO().getById( similar.getKey().split( "->" )[0] ).isAdded() );
+			similarAuthorMap.put( "similarity", similar.getKey().split( "->" )[1] );
+
+			// construct the map for the list of topics
+			List<Object> topicleveldetail = new ArrayList<Object>();
+			for ( String topic : similar.getValue() )
+			{
+				Map<String, Object> topicproportions = new LinkedHashMap<String, Object>();
+				topicproportions.put( "name", topic.split( "_-_" )[0] );
+					topicproportions.put( "value", "" );// Math.round( (
+														// Double.parseDouble(
+														// topic.split( "_-_"
+														// )[1] ) * 100 ) / 100
+														// ) );
+				topicleveldetail.add( topicproportions );
+			}
+
+			similarAuthorMap.put( "topicdetail", topicleveldetail );
+			// add into list
+			similarAuthorList.add( similarAuthorMap );
+			}
+		}
+
+		// prepare list of object map containing similarAuthor details
+		List<Map<String, Object>> similarAuthorListPaging = new ArrayList<Map<String, Object>>();
+
+		int position = 0;
+		for ( Map<String, Object> similarAuthor : similarAuthorList )
+		{
+			if ( position >= startPage && similarAuthorListPaging.size() < maxresult )
+			{
+				similarAuthorListPaging.add( similarAuthor );
+			}
+		}
 
 		// put similarAuthor to responseMap
 		responseMap.put( "countTotal", similarAuthorList.size() );
