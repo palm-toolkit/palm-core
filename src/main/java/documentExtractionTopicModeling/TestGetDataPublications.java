4b13949855c360b694fa6d00130be87f1e510e5b
Piro Lena <piro.lena@rwth-aachen.de>
2016-12-11 15:04:27 +0100

    Classes used for data extraction. Added after testing.

---
+++ src/main/java/documentExtractionTopicModeling/TestGetDataPublications.java
@@ -0,0 +1,46 @@
+package documentExtractionTopicModeling;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+import de.rwth.i9.palm.model.Author;
+import de.rwth.i9.palm.model.Publication;
+import de.rwth.i9.palm.persistence.PersistenceStrategy;
+
+public class TestGetDataPublications extends AbstractTransactionalJUnit4SpringContextTests
+{
+	@Autowired
+	private PersistenceStrategy persistenceStrategy;
+	
+	String path =  "C:/Users/Albi/Desktop/";
+
+	public void testGetDatabaseFromDatabase() throws FileNotFoundException, UnsupportedEncodingException
+	{
+		//System.out.println( "\n========== TEST 0 - Fetch publications from database ==========" );
+		List<Author> authors = persistenceStrategy.getAuthorDAO().getAll();
+		if ( !authors.isEmpty() )
+			for ( Author author : authors )
+			{
+				for ( Publication publication : author.getPublications() )
+				{
+					if ( publication.getAbstractText() != "null" )
+					{
+						PrintWriter writer = new PrintWriter( path + "Publications/Publications/" + publication.getId() + ".txt", "UTF-8" );
+						writer.print( publication.getTitle() + " " );
+						writer.print( publication.getAbstractText() );
+						writer.println();
+						writer.close();
+					}
+					else
+					{
+						continue;
+					}
+				}
+			}
+	}
+
+}
