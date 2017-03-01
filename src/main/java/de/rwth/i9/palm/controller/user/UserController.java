efccd3790f6e8760972b7d9465fc40f7967bf139
Ligia <ligia_bildea@yahoo.com>
2017-03-01 11:24:04 +0100

    created function in the controller

--- src/main/java/de/rwth/i9/palm/controller/user/UserController.java
+++ src/main/java/de/rwth/i9/palm/controller/user/UserController.java
@@ -28,7 +28,6 @@ import de.rwth.i9.palm.service.SecurityService;
 @RequestMapping( value = "/user" )
 public class UserController
 {
-
 	private static final String LINK_NAME = "user";
 	
 	@Autowired
@@ -83,6 +82,8 @@ public class UserController
 			widgets = persistenceStrategy.getWidgetDAO().getActiveWidgetByWidgetTypeAndGroup( WidgetType.USER, "user-bookmark-eventGroup" );
 		else if ( bookmarkType.equals( "circle" ) )
 			widgets = persistenceStrategy.getWidgetDAO().getActiveWidgetByWidgetTypeAndGroup( WidgetType.USER, "user-bookmark-circle" );
+		
+		System.out.println("PERSISTECE : " + persistenceStrategy);
 		// assign the model
 		model.addObject( "widgets", widgets );
 
@@ -153,6 +154,24 @@ public class UserController
 	public @ResponseBody Map<String, Object> bookmarkRemove( @PathVariable String bookmarkType, @RequestParam( value = "userId" ) final String userId, @RequestParam( value = "bookId" ) final String bookId, final HttpServletResponse response)
 	{
 		return userFeature.getUserBookmark().removeUserBookmark( bookmarkType, userId, bookId );
+	}
+	
+	@Transactional
+	@RequestMapping( value = "/publicationList", method = RequestMethod.GET )
+	public @ResponseBody Map<String, Object> getPublicationList( 
+			@RequestParam( value = "id", required = false ) final String authorId,
+			@RequestParam( value = "query", required = false ) String query, 
+			@RequestParam( value = "year", required = false ) String year,
+			@RequestParam( value = "orderBy", required = false ) String orderBy,
+			@RequestParam( value = "startPage", required = false ) Integer startPage, 
+			@RequestParam( value = "maxresult", required = false ) Integer maxresult,
+			final HttpServletResponse response)
+	{
+		if ( year == null )				year = "all";
+		if ( query == null )			query = "";
+		if ( orderBy == null )			orderBy = "date";
+		
+		return userFeature.getUserPublication().getPublicationListByAuthorId( authorId, query, year, startPage, maxresult, orderBy );
 	}
 
 }
